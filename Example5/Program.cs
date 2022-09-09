c86d8d2a513a56d069c21b988e2122e03a9160ef
Stanislav <stasvitalich1994@gmail.com>
2022-09-09 15:36:10 +0700

    Example5 has been solved

---
+++ Example5/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+int result1 = numberOne / numberTwo;  
+int result2 = numberTwo / numberOne;
+
+if (numberOne > numberTwo)
+{
+    if (result1 == numberTwo)
+    {
+        Console.WriteLine("Да");
+    }
+     else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+
+if (numberTwo > numberOne)
+{
+    if (result2 == numberOne)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
