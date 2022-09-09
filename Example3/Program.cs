a2afb6c43eec0b152d31feaa97e03d54586cf589
Stanislav <stasvitalich1994@gmail.com>
2022-09-09 13:41:50 +0700

    Example3 was completed

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+int result = numberOne % numberTwo;
+
+if (result == 0) Console.WriteLine("Кратно");
+else
+{
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
