b239e4c497eae8249a0110a540816a37dc3d5a76
Stanislav <stasvitalich1994@gmail.com>
2022-09-09 13:07:38 +0700

    The folder for Example 3 has just been created

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Инициализируем ввод случайного трёхзначного числа
+int x = new Random ().Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное число {x}");
+
+int firstDigit = x / 100;
+Console.WriteLine($"Первая цифра: {firstDigit}");
+
+int lastDigit = x % 10;
+Console.WriteLine($"Последняя цифра: {lastDigit}");
+
+int finalDigit = firstDigit * 10 + lastDigit;
+Console.WriteLine($"Итоговая цифра: {finalDigit}");
