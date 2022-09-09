9f97024b90ec9866d0d2c876df04cf092b39b20a
Stanislav <stasvitalich1994@gmail.com>
2022-09-09 12:13:23 +0700

    The folder Example2 has been created

--- Example1/Program1.cs
+++ Example1/Program1.cs
@@ -3,14 +3,18 @@
 //78 -> 8
 
 int x = new Random().Next(10, 100);
-int x1 = x/10;
-int x2 = x%10;
-if (x1>x2) Console.WriteLine(x1);
-else
+Console.WriteLine($"Сгенерировано случайное число {x}");
+
+
+void CompareDigits ()
 {
-    Console.WriteLine(x2);
+    int x1 = x/10;
+    int x2 = x%10;
+    if (x1>x2) Console.WriteLine("Первая цифра больше");
+    else
+    {
+        Console.WriteLine("Вторая цифра больше");
+    }
 }
-Console.WriteLine(x);
 
-
-
+CompareDigits();
